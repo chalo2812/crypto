a3816c04e660813682cff3a26bb7dc0512a392da
Gonzalo <gonzalosola@gmail.com>
2022-07-07 09:22:44 -0300

    ajustes

--- crypto-back/src/main/java/ar/crypto/back/controller/BinanceController.java
+++ crypto-back/src/main/java/ar/crypto/back/controller/BinanceController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
