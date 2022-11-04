b1a025d0db6a16fb75d97b12046d6dbe8820e526
Gonzalo <gonzalosola@gmail.com>
2022-11-04 00:43:02 -0300

    Modificacion en la web

--- crypto-front/src/App.js
+++ crypto-front/src/App.js
@@ -1,11 +1,10 @@
 //@package
 //eslint-disable-next-line
 import React, {useState, useEffect} from 'react';
-
 //JS - Components
 import Header from './componentes/header/Header.js';
+import Crypto from './componentes/crypto/Crypto.js';
 import './index.js';
-
 //Style
 import './App.css';
 import './index.css';
@@ -15,6 +14,7 @@ export default function App()  {
   return (
     <div className="App">
       <Header/>
+      <Crypto/>
     </div>
   );
 }
